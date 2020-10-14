06442b6fb0f4f67942c6358367fcc956e52acc64
Rafael Coutinho <r.bcoutinho@hotmail.com>
2020-10-14 11:28:43 -0300

    first commit

---
+++ sorteio.py
@@ -0,0 +1,14 @@
+import random
+
+sorteado = random.randrange(1,5)
+
+print(sorteado)
+
+if sorteado == 1:
+    print( "Paulo")
+elif sorteado == 2:
+    print("Juliana")
+elif sorteado == 3:
+    print("Tamires")
+elif sorteado == 4:
+    print("Alfredo")
